0d15bba51e4f6526136ccee7a348366145f2c7ea
pfyy <112098515+pfyy@users.noreply.github.com>
2025-10-09 21:40:18 +0800

    update chrono

--- src/mods/chronosphere/main.py
+++ src/mods/chronosphere/main.py
@@ -53,8 +53,9 @@ def copy_zonemap_node(mgr: ManifestMerger):
 def main():
     mgr = ManifestMerger(
         "chronosphere",
-        Resource("2.6.41", "25-09-28-12-13-16_6485b3"),
+        Resource("2.6.61", "25-09-29-16-32-32_00dc6f"),
         [
+            Resource("2.6.41", "25-09-28-12-13-16_6485b3"),
             Resource("2.6.21", "25-08-25-23-45-59_81c7ff"),
             Resource("2.6.01", "25-07-19-05-16-54_1e71a6"),
             Resource("2.5.80", "25-06-26-04-47-55_47709b"),
